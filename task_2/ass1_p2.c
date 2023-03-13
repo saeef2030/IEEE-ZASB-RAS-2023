b83f78b442b675195ae1f1b4a21bc97d4636e2cd
SEIF.M <120063984+saeef2030@users.noreply.github.com>
2023-03-13 20:35:27 +0200

    Update ass1_p2.c

--- task_2/ass1_p2.c
+++ task_2/ass1_p2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 void main(){
-    int w =0x3E2F;
+    int w =0x3EF110;
     int *ptr = &w;
-    printf("ptr= %d & *ptr= %u\n",ptr,*ptr);
-    int y=*ptr++;               //*ptr++ >>>>effectively dereferences the pointer, then increments the pointer<<<<<<<
-    printf("ptr= %d & *ptr= %u & y=%d\n",ptr,*ptr,y);
-    int x=++*ptr;              //++*ptr >>>>effectively dereferences the pointer, then increments dereferenced value<<<<<
-    printf("ptr= %d & *ptr= %u & x=%d\n",ptr,*ptr,x);
-    int r=*++ptr;             //*++ptr >>>>effectively increments the pointer, then dereferences the pointer<<<<<
-    printf("ptr= %d & *ptr= %u & r=%d\n",ptr,*ptr,r);
+    printf("ptr= %x & *ptr= %x\n",ptr,*ptr);
+
+    *ptr++;               //*ptr++ >>>>effectively dereferences the pointer, then increments the pointer<<<<<<<
+    printf("ptr= %x & *ptr= %x \n",ptr,*ptr);
+ 
+    ++*ptr;              //++*ptr >>>>effectively dereferences the pointer, then increments dereferenced value<<<<<
+    printf("ptr= %x & *ptr= %x \n",ptr,*ptr);
+
+    *++ptr;             //*++ptr >>>>effectively increments the pointer, then dereferences the pointer<<<<<
+    printf("ptr= %x & *ptr= %x \n",ptr,*ptr);
 }
  
