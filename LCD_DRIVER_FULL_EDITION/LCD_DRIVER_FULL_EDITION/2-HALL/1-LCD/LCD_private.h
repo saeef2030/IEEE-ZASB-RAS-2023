f543530aa11f3584ecef0a4a2e3b23111d412fb9
SEIF.M <120063984+saeef2030@users.noreply.github.com>
2023-05-10 08:27:50 +0300

    LCD_DRIVER_FULL_EDITION
    LCD_DRIVER_FULL_EDITION

---
+++ LCD_DRIVER_FULL_EDITION/LCD_DRIVER_FULL_EDITION/2-HALL/1-LCD/LCD_private.h
@@ -0,0 +1,31 @@
+/*
+ *      SWC       : LCD
+ *      version   : 1.0
+ *      Created on: May 3, 2023
+ *      Author    : SEIF.M
+ */
+
+#ifndef LCD_PRIVATE_H
+#define LCD_PRIVATE_H
+
+#define ADDRESS_OF_CGRAM            0x40
+#define LCD_u8_MODE_8_BITS            1
+#define LCD_u8_MODE_4_BITS            2
+
+
+
+
+#define CHAR_0                          0
+#define CHAR_1                          1                 
+#define CHAR_2                          2
+#define CHAR_3                          3
+#define CHAR_4                          4
+#define CHAR_5                          5
+#define CHAR_6                          6
+#define CHAR_7                          7
+
+#define SHIFT_4BIT                      4
+
+#define MAX_CHARACTERS                  8
+
+#endif
